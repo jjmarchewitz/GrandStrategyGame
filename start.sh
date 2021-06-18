7933bcb582cb4c21e30fa5442962a77c2cc690e4
Jake Marchewitz <jjmarchewitz@gmail.com>
2021-06-18 19:44:36 -0400

    Updated start.sh with print statements

--- start.sh
+++ start.sh
@@ -1,9 +1,15 @@
 function start() {
+    echo "Pulling latest version from Git..."
+    git pull
     if [[ "$OSTYPE" == "msys"* ]]; then
+        echo "Updating python requirements..."
         python -m pip install -r requirements.txt
+        echo "Opening game..."
         python main.py
     elif [[ "$OSTYPE" == "darwin"* ]]; then
+        echo "Updating python requirements..."
         python3 -m pip install -r requirements.txt
+        echo "Opening game..."
         python3 main.py
     fi
 }
