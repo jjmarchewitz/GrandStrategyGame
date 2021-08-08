06371f7b438710be8061d6c2167ec58bc23d58a5
Jake Marchewitz <7871951+jjmarchewitz@users.noreply.github.com>
2021-08-07 22:29:10 -0400

    Added some more folders to file_paths.py

--- engine/file_paths.py
+++ engine/file_paths.py
@@ -4,6 +4,19 @@ def get_root_folder():
     return os.getcwd()
 
 def get_font_folder():
-    game_folder = get_root_folder()
-    font_folder = os.path.join(game_folder, "assets", "fonts")
-    return font_folder
+    return os.path.join(get_root_folder(), "assets", "fonts")
+
+def get_images_folder():
+    return os.path.join(get_root_folder(), "assets", "images")
+
+def get_sounds_folder():
+    return os.path.join(get_root_folder(), "assets", "sounds")
+
+def get_flavor_folder():
+    return os.path.join(get_root_folder(), "flavor")
+
+def get_map_data_folder():
+    return os.path.join(get_root_folder(), "map") 
+
+def get_save_game_folder():
+    return os.path.join(get_root_folder(), "save_games")
